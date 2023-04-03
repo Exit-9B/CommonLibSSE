76fba14e856bfe3c91cd8743b6befdb1da182f2e
Parapets <southwest85b@gmail.com>
2023-04-03 12:53:27 +0100

    Create Init functions for InputEvents

--- include/RE/C/CharEvent.h
+++ include/RE/C/CharEvent.h
@@ -11,6 +11,11 @@ namespace RE
 
 		~CharEvent() override;  // 00
 
+		void Init(std::uint32_t a_keyCode)
+		{
+			keyCode = a_keyCode;
+		}
+
 		std::uint32_t keyCode;  // 18
 		std::uint32_t pad1C;    // 1C
 	};
--- src/RE/B/BSInputEventQueue.cpp
+++ src/RE/B/BSInputEventQueue.cpp
@@ -8,120 +8,6 @@ namespace RE
 		return *singleton;
 	}
 
-	void BSInputEventQueue::AddButtonEvent(INPUT_DEVICE a_device, std::int32_t a_id, float a_value, float a_duration)
-	{
-		if (buttonEventCount < MAX_BUTTON_EVENTS) {
-			auto& cachedEvent = buttonEvents[buttonEventCount];
-			cachedEvent.value = a_value;
-			cachedEvent.heldDownSecs = a_duration;
-			cachedEvent.device = a_device;
-			cachedEvent.idCode = a_id;
-			cachedEvent.userEvent = {};
-#ifdef SKYRIMVR
-			cachedEvent.unkVR28 = -1;
-#endif
-
-			PushOntoInputQueue(&cachedEvent);
-			++buttonEventCount;
-		}
-	}
-
-	void BSInputEventQueue::AddCharEvent(std::uint32_t a_keyCode)
-	{
-		if (charEventCount < MAX_CHAR_EVENTS) {
-			auto& cachedEvent = charEvents[charEventCount];
-			cachedEvent.keyCode = a_keyCode;
-
-			PushOntoInputQueue(&cachedEvent);
-			++charEventCount;
-		}
-	}
-
-	void BSInputEventQueue::AddMouseMoveEvent(std::int32_t a_mouseInputX, std::int32_t a_mouseInputY)
-	{
-		if (mouseEventCount < MAX_MOUSE_EVENTS) {
-			auto& cachedEvent = mouseEvents[mouseEventCount];
-			cachedEvent.mouseInputX = a_mouseInputX;
-			cachedEvent.mouseInputY = a_mouseInputY;
-			cachedEvent.userEvent = {};
-
-			PushOntoInputQueue(&cachedEvent);
-			++mouseEventCount;
-		}
-	}
-
-	void BSInputEventQueue::AddThumbstickEvent(ThumbstickEvent::InputType a_id, float a_xValue, float a_yValue)
-	{
-		if (thumbstickEventCount < MAX_THUMBSTICK_EVENTS) {
-			auto& cachedEvent = thumbstickEvents[thumbstickEventCount];
-			cachedEvent.xValue = a_xValue;
-			cachedEvent.yValue = a_yValue;
-			cachedEvent.device = INPUT_DEVICE::kGamepad;
-			cachedEvent.idCode = a_id;
-			cachedEvent.userEvent = {};
-
-			PushOntoInputQueue(&cachedEvent);
-			++thumbstickEventCount;
-		}
-	}
-
-	void BSInputEventQueue::AddConnectEvent(INPUT_DEVICE a_device, bool a_connected)
-	{
-		if (connectEventCount < MAX_CONNECT_EVENTS) {
-			auto& cachedEvent = connectEvents[connectEventCount];
-			cachedEvent.device = a_device;
-			cachedEvent.connected = a_connected;
-
-			PushOntoInputQueue(&cachedEvent);
-			++connectEventCount;
-		}
-	}
-
-	void BSInputEventQueue::AddKinectEvent(const BSFixedString& a_userEvent, const BSFixedString& a_heard)
-	{
-		if (kinectEventCount < MAX_KINECT_EVENTS) {
-			auto& cachedEvent = kinectEvents[kinectEventCount];
-			cachedEvent.userEvent = a_userEvent;
-			cachedEvent.heard = a_heard;
-
-			PushOntoInputQueue(&cachedEvent);
-			++kinectEventCount;
-		}
-	}
-
-#ifdef SKYRIMVR
-	void BSInputEventQueue::AddButtonEvent(INPUT_DEVICE a_device, std::int32_t a_arg2, std::int32_t a_id, float a_value, float a_duration)
-	{
-		if (buttonEventCount < MAX_BUTTON_EVENTS) {
-			auto& cachedEvent = buttonEvents[buttonEventCount];
-			cachedEvent.value = a_value;
-			cachedEvent.heldDownSecs = a_duration;
-			cachedEvent.device = a_device;
-			cachedEvent.idCode = a_id;
-			cachedEvent.userEvent = {};
-			cachedEvent.unkVR28 = a_arg2;
-
-			PushOntoInputQueue(&cachedEvent);
-			++buttonEventCount;
-		}
-	}
-
-	void BSInputEventQueue::AddThumbstickEvent(ThumbstickEvent::InputType a_id, INPUT_DEVICE a_device, float a_xValue, float a_yValue)
-	{
-		if (thumbstickEventCount < MAX_THUMBSTICK_EVENTS) {
-			auto& cachedEvent = thumbstickEvents[thumbstickEventCount];
-			cachedEvent.xValue = a_xValue;
-			cachedEvent.yValue = a_yValue;
-			cachedEvent.device = a_device;
-			cachedEvent.idCode = a_id;
-			cachedEvent.userEvent = {};
-
-			PushOntoInputQueue(&cachedEvent);
-			++thumbstickEventCount;
-		}
-	}
-#endif
-
 	void BSInputEventQueue::PushOntoInputQueue(InputEvent* a_event)
 	{
 		if (!queueHead) {
@@ -144,6 +30,11 @@ namespace RE
 		mouseEventCount = 0;
 		charEventCount = 0;
 		buttonEventCount = 0;
+#ifdef SKYRIMVR
+		vrTouchpadSwipeEventCount = 0;
+		vrTouchpadPositionEventCount = 0;
+		unkVR01C = 0;
+#endif
 		queueTail = nullptr;
 		queueHead = nullptr;
 	}
