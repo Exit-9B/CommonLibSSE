1eb8f6b897c2caf2161d69b96b71c953d52964fb
Parapets <southwest85b@gmail.com>
2023-04-04 11:49:06 +0100

    Code cleanup

--- src/RE/B/BSInputEventQueue.cpp
+++ src/RE/B/BSInputEventQueue.cpp
@@ -8,6 +8,128 @@ namespace RE
 		return *singleton;
 	}
 
+	template <>
+	ButtonEvent* BSInputEventQueue::GetCachedEvent<ButtonEvent>()
+	{
+		if (buttonEventCount < MAX_BUTTON_EVENTS) {
+			return &buttonEvents[buttonEventCount];
+		}
+
+		return nullptr;
+	}
+
+	template <>
+	CharEvent* BSInputEventQueue::GetCachedEvent<CharEvent>()
+	{
+		if (charEventCount < MAX_CHAR_EVENTS) {
+			return &charEvents[charEventCount];
+		}
+
+		return nullptr;
+	}
+
+	template <>
+	MouseMoveEvent* BSInputEventQueue::GetCachedEvent<MouseMoveEvent>()
+	{
+		if (mouseEventCount < MAX_MOUSE_EVENTS) {
+			return &mouseEvents[mouseEventCount];
+		}
+
+		return nullptr;
+	}
+
+	template <>
+	ThumbstickEvent* BSInputEventQueue::GetCachedEvent<ThumbstickEvent>()
+	{
+		if (thumbstickEventCount < MAX_THUMBSTICK_EVENTS) {
+			return &thumbstickEvents[thumbstickEventCount];
+		}
+
+		return nullptr;
+	}
+
+	template <>
+	DeviceConnectEvent* BSInputEventQueue::GetCachedEvent<DeviceConnectEvent>()
+	{
+		if (connectEventCount < MAX_CONNECT_EVENTS) {
+			return &connectEvents[connectEventCount];
+		}
+
+		return nullptr;
+	}
+
+#ifdef SKYRIMVR
+	template <>
+	VrWandTouchpadPositionEvent* BSInputEventQueue::GetCachedEvent<VrWandTouchpadPositionEvent>()
+	{
+		if (vrTouchpadPositionEvent < MAX_VR_TOUCHPAD_POSITION_EVENTS) {
+			return &vrTouchpadPositionEvents[vrTouchpadPositionEventCount];
+		}
+
+		return nullptr;
+	}
+
+	template <>
+	VrWandTouchpadSwipeEvent* BSInputEventQueue::GetCachedEvent<VrWandTouchpadSwipeEvent>()
+	{
+		if (vrTouchpadSwipeEvent < MAX_VR_TOUCHPAD_SWIPE_EVENTS) {
+			return &vrTouchpadSwipeEvents[vrTouchpadSwipeEventCount];
+		}
+
+		return nullptr;
+	}
+#endif
+
+	template <>
+	void BSInputEventQueue::AdvanceCount<ButtonEvent>()
+	{
+		++buttonEventCount;
+	}
+
+	template <>
+	void BSInputEventQueue::AdvanceCount<CharEvent>()
+	{
+		++charEventCount;
+	}
+
+	template <>
+	void BSInputEventQueue::AdvanceCount<MouseMoveEvent>()
+	{
+		++mouseEventCount;
+	}
+
+	template <>
+	void BSInputEventQueue::AdvanceCount<ThumbstickEvent>()
+	{
+		++thumbstickEventCount;
+	}
+
+	template <>
+	void BSInputEventQueue::AdvanceCount<DeviceConnectEvent>()
+	{
+		++connectEventCount;
+	}
+
+	template <>
+	void BSInputEventQueue::AdvanceCount<KinectEvent>()
+	{
+		++kinectEventCount;
+	}
+
+#ifdef SKYRIMVR
+	template <>
+	void BSInputEventQueue::AdvanceCount<VrWandTouchpadPositionEvent>()
+	{
+		++vrTouchpadPositionEventCount;
+	}
+
+	template <>
+	void BSInputEventQueue::AdvanceCount<VrWandTouchpadSwipeEvent>()
+	{
+		++vrTouchpadSwipeEventCount;
+	}
+#endif
+
 	void BSInputEventQueue::PushOntoInputQueue(InputEvent* a_event)
 	{
 		if (!queueHead) {
