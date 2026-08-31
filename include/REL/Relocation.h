553fd967d1bf609c286f6093b268c9580468bc68
Parapets <southwest85b@gmail.com>
2026-08-31 15:59:34 +0100

    chore: disable inlining of match_or_fail

--- include/REL/Relocation.h
+++ include/REL/Relocation.h
@@ -1289,7 +1289,7 @@ retry:
 				return this->match(*reinterpret_cast<const std::byte(*)[sizeof...(Rules)]>(a_address));
 			}
 
-			void match_or_fail(std::uintptr_t a_address, std::source_location a_loc = std::source_location::current()) const noexcept
+			[[msvc::noinline]] void match_or_fail(std::uintptr_t a_address, std::source_location a_loc = std::source_location::current()) const noexcept
 			{
 				if (!this->match(a_address)) {
 					const auto version = Module::get().version();
